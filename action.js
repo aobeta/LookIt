260720b3fc36ecd8b9e2ff5e3198f4f6fb72d52f
aobeta <choobey21@gmail.com>
2016-05-04 22:42:36 -0700

    made progress on browser action

--- action.js
+++ action.js
@@ -1,37 +1,63 @@
-function check(e,val){
-if(e.which === 13 || e.keyCode === 13){
-    var val = e.target.value
-    var url = "https://wordsapiv1.p.mashape.com/words/"+val;
-    $.ajax({
-      type: "GET",
-      beforeSend: function(request){
-        request.setRequestHeader("X-Mashape-Key", "Abdm4OXNIDmshklXZq801hMlC2rcp1");
-      },
-      url: url,
-      dataType: "json",
-      success : function(data){
-          $("#content").css("display", "block")
-          var def = JSON.parse(data);
-          var results = def.results;
-          for(obj in results){
-            $("#content").text(obj.definition);
-          }
-      },
-      error: function(xhr,textStatus){
-        $("#content").text(textStatus);
-      }
-    });
 
-  }
-
-}
 
 function checkE(e){
+
   if(e.which === 13 || e.keyCode === 13){
-    document.getElementById("content").innerHTML = "YOU PRESSED ENTER";
+
+    var content= document.getElementById("content");
+    
+    clearContent(content);
+
+    content.style.opacity = "1";
+    content.style.height = "auto";
+    //content.textContent = e.target.value;
+
+    //getting request
+    var request = new XMLHttpRequest();
+    var url = "https://wordsapiv1.p.mashape.com/words/"+e.target.value;
+    request.open("GET",url , true );
+    request.setRequestHeader("X-Mashape-Key", "Abdm4OXNIDmshklXZq801hMlC2rcp1UoSzwjsnA6bRIeYXsbIf");
+    //request.responseType = "json";
+
+    request.onreadystatechange = function (){
+
+
+      if(request.readyState == 4 && request.status == 200 ){
+        var data = JSON.parse(request.responseText);
+        var res = data.results;
+        var max = res.length > 3 ?   3:res.length;
+        for(var i = 0; i < max; i++){
+          var span = document.createElement("span");
+          span.textContent = res[i].partOfSpeech;
+          span.style.color = "#FF0000";
+
+          content.appendChild(span);
+          content.appendChild(document.createElement("br"));
+          content.appendChild(document.createTextNode(res[i].definition));
+          content.appendChild(document.createElement("br"));
+          content.appendChild(document.createElement("br"));
+
+        }
+      }else if (request.readyState == 4 && request.status == 404) {
+        var h3 = document.createElement("H3");
+        h3.style.color = "red";
+        var str = e.target.value+" is not a word please enter a proper english word";
+        h3.appendChild(document.createTextNode(str));
+        content.appendChild(h3);
+      }
+
+    };
+    request.send();
   }
 }
 
-fuction display(){
-    document.getElementById("content").innerHTML = "YOU PRESSED ENTER";
+
+function clearContent(content){
+  while (content.hasChildNodes()) {
+    content.removeChild(content.childNodes[0]);
+  }
 }
+
+
+//document.getElementById("clicka").onclick = display;
+document.getElementById("search").onkeypress = checkE;
